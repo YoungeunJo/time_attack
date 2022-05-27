820ac4ee6396c79bb414cd7cf67823e5f983dfbe
YoungeunJo <youngeun.jo1494@gmail.com>
2022-05-27 21:39:46 +0900

    차집합 update

--- 0527/src/main/java/com/example/_0527/Main.java
+++ 0527/src/main/java/com/example/_0527/Main.java
@@ -4,21 +4,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
-class Calculator
-{
-    public Calculator() {}
+class Calculator {
+    public Calculator() {
+    }
+
     // Sum of A n B
-    public int intersection(int[] A, int[] B)
-    {
+    public int intersection(int[] A, int[] B) {
         int result = 0;
 
-        for(int aNum: A){
-            for(int bNum: B){
-                if (aNum==bNum){
-                    System.out.println(aNum);
-                    result+=aNum;
-                    System.out.println("result"+result);
+        for (int aNum : A) {
+            for (int bNum : B) {
+                if (aNum == bNum) {
+                    result += aNum;
                 }
             }
         }
@@ -26,44 +25,62 @@ class Calculator
     }
 
     // Sum of A - B
-    public int differenceOfSet(int[] A, int[] B)
-    {
+    public int differenceOfSet(int[] A, int[] B) {
         int result = 0;
-        // todo
+
+        ArrayList<Integer> arrayListA = new ArrayList<>();
+        for (int temp : A) {
+            arrayListA.add(temp);
+        }
+        ArrayList<Integer> interArray = new ArrayList<>();
+
+        for (int aNum : A) {
+            for (int bNum : B) {
+                if (aNum == bNum) {
+                    interArray.add(aNum);
+                }
+            }
+        }
+        for(int items : interArray) {
+             arrayListA.remove(Integer.valueOf(items));
+        }
+        for(int items: arrayListA){
+            result += items;
+        }
         return result;
     }
 }
 
-class Minesweeper
-{
+class Minesweeper {
     public static int MAP_X = 10;
     public static int MAP_Y = 10;
     private int[][] map;
     private int numOfpick;
-    public Minesweeper(int[][] map) { this.map = map; this.numOfpick = 0;}
 
-    public int pick(int x, int y)
-    {
+    public Minesweeper(int[][] map) {
+        this.map = map;
+        this.numOfpick = 0;
+    }
+
+    public int pick(int x, int y) {
         int numOfMine = 0;
         // todo
         return numOfMine;
     }
 
-    public int getNumOfpick()
-    {
+    public int getNumOfpick() {
         return numOfpick;
     }
 
-    public boolean checkMap()
-    {
+    public boolean checkMap() {
         // todo
         return false;
     }
 
     public void printMap() {
-        for(int i = 0; i < MAP_X; i++) {
-            for(int j = 0; j < MAP_Y; j++) {
-                System.out.print(map[i][j]+" ");
+        for (int i = 0; i < MAP_X; i++) {
+            for (int j = 0; j < MAP_Y; j++) {
+                System.out.print(map[i][j] + " ");
             }
             System.out.println();
         }
@@ -72,23 +89,22 @@ class Minesweeper
 
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println("intersection = "+ calculator.intersection(new int[]{1,2,4,11,6,7,5,14,19,16},new int[]{2,9,8,4,11,19,15,12}));
-        System.out.println("differenceOfSet = "+ calculator.differenceOfSet(new int[]{1,2,4,11,6,7,5,14,19,16},new int[]{2,9,8,4,11,19,15,12}));
+        System.out.println("intersection = " + calculator.intersection(new int[]{1, 2, 4, 11, 6, 7, 5, 14, 19, 16}, new int[]{2, 9, 8, 4, 11, 19, 15, 12}));
+        System.out.println("differenceOfSet = " + calculator.differenceOfSet(new int[]{1, 2, 4, 11, 6, 7, 5, 14, 19, 16}, new int[]{2, 9, 8, 4, 11, 19, 15, 12}));
 
         int[][] map = {
-                {0,0,1,0,0,0,0,1,0,0},
-                {0,0,0,0,0,1,0,1,0,0},
-                {0,0,1,0,0,0,0,0,0,1},
-                {0,1,0,0,1,0,0,0,0,0},
-                {1,0,0,0,1,0,0,1,0,0},
-                {0,1,0,1,0,0,0,0,0,0},
-                {0,0,1,0,1,0,0,1,0,0},
-                {0,1,0,0,0,0,1,0,0,1},
-                {1,0,1,0,1,0,0,0,0,0},
-                {0,0,0,0,0,0,1,0,0,0}
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0, 1, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0, 0, 0, 1},
+                {0, 1, 0, 0, 1, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 1, 0, 0, 1, 0, 0},
+                {0, 1, 0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 1, 0, 0, 1, 0, 0},
+                {0, 1, 0, 0, 0, 0, 1, 0, 0, 1},
+                {1, 0, 1, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 1, 0, 0, 0}
         };
 
         Minesweeper minesweeper = new Minesweeper(map);
@@ -97,20 +113,20 @@ public class Main {
         System.out.println("Minesweeper start!!!");
         System.out.println("----------------------------------------------------");
         minesweeper.printMap();
-        while(minesweeper.checkMap()) {
+        while (minesweeper.checkMap()) {
             System.out.print("x(0~9) : ");
             int x = scanner.nextInt();
             System.out.print("y(0~9) : ");
             int y = scanner.nextInt();
 
-            if(x >= minesweeper.MAP_X || y >= minesweeper.MAP_Y || x < 0 || y < 0) break;
+            if (x >= minesweeper.MAP_X || y >= minesweeper.MAP_Y || x < 0 || y < 0) break;
 
-            int numOfMine = minesweeper.pick(Minesweeper.MAP_Y - y -1, x);
-            if(numOfMine == -1) {
+            int numOfMine = minesweeper.pick(Minesweeper.MAP_Y - y - 1, x);
+            if (numOfMine == -1) {
                 System.out.println("Mine has exploded!!!");
                 break;
             } else {
-                System.out.println("There's a mine around : "+numOfMine);
+                System.out.println("There's a mine around : " + numOfMine);
                 minesweeper.printMap();
             }
         }
